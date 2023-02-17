ad28febf5b30fa37f377f8eab2d17661c67b9a0d
CSardo01 <sardc042@newschool.edu>
2023-02-16 20:51:42 -0500

    added covers

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const unicornDown = document.querySelectorAll(".unicorn-down")
+const unicornUp = document.querySelectorAll(".unicorn-up")
+const unicornObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("show", entry.isIntersecting)
+    })
+},{
+    root: document,
+    rootMargin: '-40% 0% -40% 0%',
+})
+
+unicornDown.forEach(unicornDown => {unicornObserver.observe(unicornDown)})
+unicornUp.forEach(unicornUp => {unicornObserver.observe(unicornUp)})
+//---
+
 const hRule = document.querySelectorAll(".h-rule")
 const deckard = document.querySelectorAll(".deckard")
 const rachel = document.querySelectorAll(".rachel")
@@ -18,6 +33,7 @@ deckard.forEach(deckard => { observer.observe(deckard) })
 rachel.forEach(rachel => { observer.observe(rachel) })
 tyrell.forEach(tyrell => { observer.observe(tyrell) })
 tyrellConclusion.forEach(tyrellConclusion => { observer.observe(tyrellConclusion) })
+//---
 
 const deckardConclusion = document.querySelectorAll(".deckard-conclusion")
 
@@ -25,10 +41,11 @@ const observerTwo = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         entry.target.classList.toggle("show", entry.isIntersecting);
         document.querySelector(".background-2").classList.toggle("show", entry.isIntersecting)
+        // if (entry.isIntersecting) observer.unobserve(entry.target)
     })
 }, {
     root: document,
-    rootMargin: '-20% 0% -50% 0%',
+    rootMargin: '200% 0% -50% 0%',
 })
 
 deckardConclusion.forEach(deckardConclusion => { observerTwo.observe(deckardConclusion)})
