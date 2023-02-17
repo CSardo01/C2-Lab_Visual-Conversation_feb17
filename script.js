4acd3a05021de976ea87d85f9175119666345979
CSardo01 <sardc042@newschool.edu>
2023-02-17 10:56:36 -0500

    Unicorn Button Edits

--- script.js
+++ script.js
@@ -4,14 +4,29 @@ const unicornObserver = new IntersectionObserver(entries => {
     entries.forEach(entry => {
         entry.target.classList.toggle("show", entry.isIntersecting)
     })
-},{
+}, {
     root: document,
     rootMargin: '-40% 0% -40% 0%',
 })
 
-unicornDown.forEach(unicornDown => {unicornObserver.observe(unicornDown)})
-unicornUp.forEach(unicornUp => {unicornObserver.observe(unicornUp)})
+unicornDown.forEach(unicornDown => { unicornObserver.observe(unicornDown) })
+unicornUp.forEach(unicornUp => { unicornObserver.observe(unicornUp) })
 //---
+
+const rachelTop = document.querySelectorAll(".rachel-top")
+
+const rachelTopObserver = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        entry.target.classList.toggle("show", entry.isIntersecting)
+    })
+}, {
+    root: document,
+    rootMargin: '0% 0% -20% 0%',
+})
+
+rachelTop.forEach(rachelTop => { rachelTopObserver.observe(rachelTop) })
+//---
+
 
 const hRule = document.querySelectorAll(".h-rule")
 const deckard = document.querySelectorAll(".deckard")
@@ -28,12 +43,14 @@ const observer = new IntersectionObserver(entries => {
     rootMargin: '-20% 0% -20% 0%',
 })
 
-hRule.forEach(hRule => {observer.observe(hRule)})
+hRule.forEach(hRule => { observer.observe(hRule) })
 deckard.forEach(deckard => { observer.observe(deckard) })
 rachel.forEach(rachel => { observer.observe(rachel) })
 tyrell.forEach(tyrell => { observer.observe(tyrell) })
 tyrellConclusion.forEach(tyrellConclusion => { observer.observe(tyrellConclusion) })
 //---
+
+
 
 const deckardConclusion = document.querySelectorAll(".deckard-conclusion")
 
@@ -48,7 +65,7 @@ const observerTwo = new IntersectionObserver(entries => {
     rootMargin: '200% 0% -50% 0%',
 })
 
-deckardConclusion.forEach(deckardConclusion => { observerTwo.observe(deckardConclusion)})
+deckardConclusion.forEach(deckardConclusion => { observerTwo.observe(deckardConclusion) })
 
 
 
